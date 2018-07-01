f76cb4372179dce13c9b557b153d9026cd0e7221
DESKTOP-9OD4B8H\ScottLin <yeah@yeah.com>
2018-07-01 23:41:03 +0800

    Adding async mode Adding Timer to process sync mode

--- PingTool/Form1.cs
+++ PingTool/Form1.cs
@@ -11,6 +11,7 @@ namespace PingTool {
     public partial class Form1 : Form {
 
         private PingToolDll.PingToolDll gPingToolDll;
+        System.Timers.Timer gTimer;
 
         public Form1() {
             InitializeComponent();
@@ -20,19 +21,55 @@ namespace PingTool {
 
         private void Init() {
             gPingToolDll = new PingToolDll.PingToolDll();
+            gTimer = new System.Timers.Timer(10000);
+            gTimer.Elapsed += GTimer_Elapsed;
+        }
+
+        private void GTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            bool rt = false;
+
+            var stringIP = textBox1.Text;
+            rt = gPingToolDll.SyncCheck(stringIP, 5000);
+
+            if (rt == true)
+            {
+                gTimer.Stop();
+                MessageBox.Show("Conneted");
+            }
+            else
+            {
+                gTimer.Stop();
+                MessageBox.Show("Disconnected");
+            }
         }
 
         private void button1_Click(object sender, EventArgs e) {
             var stringIP = textBox1.Text;
-            bool rt = false;
 
-            rt = gPingToolDll.Check(stringIP, 5000);
+            gPingToolDll.AsyncCheck(stringIP, 5000);
+            MessageBox.Show("Is Async Ping!!");
+        }
 
-            if(rt == true) {
-                label2.Text = "Result: Connected";
-            } else {
-                label2.Text = "Result: Disconnected";
-            }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //bool rt = false;
+
+            //var stringIP = textBox1.Text;
+            //rt = gPingToolDll.SyncCheck(stringIP, 5000);
+            gTimer.Start();
+            MessageBox.Show("Is Sync Ping By Timer!!");
+
+            //if(rt == true)
+            //{
+            //    MessageBox.Show("Conneted");
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Disconnected");
+            //}
+
+            
         }
     }
 }
